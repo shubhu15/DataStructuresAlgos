057c8fb25ba7789757cd242502d265279ad1d723
Shubhangi Pandey <35404670+shubhu15@users.noreply.github.com>
2025-11-09 21:06:15 -0800

    Time: 100 ms (28.97%) | Memory: 19 MB (10.06%) - LeetSync

---
+++ 1340-the-dining-philosophers/the-dining-philosophers.py
@@ -0,0 +1,37 @@
+import threading
+
+class DiningPhilosophers:
+
+    def __init__(self):
+        self.lock = [ threading.Semaphore(1) for _ in range(5)]
+        self.sema = threading.Semaphore(4)
+
+    # call the functions directly to execute, for example, eat()
+    def wantsToEat(self,
+                   philosopher: int,
+                   pickLeftFork: 'Callable[[], None]',
+                   pickRightFork: 'Callable[[], None]',
+                   eat: 'Callable[[], None]',
+                   putLeftFork: 'Callable[[], None]',
+                   putRightFork: 'Callable[[], None]') -> None:
+
+                   with self.sema:
+                    l= philosopher
+                    r= (philosopher +1) % 5
+                    self.lock[l].acquire()
+                    self.lock[r].acquire()
+                    pickLeftFork()
+                    pickRightFork()
+                    eat()
+                    putLeftFork()
+                    putRightFork()
+                    self.lock[l].release()
+                    self.lock[r].release()
+                    
+
+
+        
+
+
+
+        
