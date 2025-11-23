88b003c8bb4b1aceacf0faa6c437cb7bd9345ef2
Shubhangi Pandey <35404670+shubhu15@users.noreply.github.com>
2025-11-22 19:44:28 -0800

    Time: 3 ms (81.40%) | Memory: 17.8 MB (70.67%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -8,28 +8,29 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    que.append((i, j))
+                    que.append((i, j,0))
                 if grid[i][j]==1:
                     fresh_or +=1
         if fresh_or == 0:
             return 0
 
-        time=-1   
+        time=0   
         dirc = [(1,0), (0,1), (-1,0), (0,-1)]   
         while que:
-            l = len(que)
+            # l = len(que)
             
-            for i in range(l):
+            # for i in range(l):
                 
-                curri, currj = que.popleft()
-                for r,c in dirc:
-                    dr = r+curri
-                    dc= c+currj
-                    if 0<= dr < m and 0<= dc<n and grid[dr][dc]==1:
-                        grid[dr][dc]=2
-                        que.append((dr, dc))
-                        fresh_or-=1
-            time+=1
+            curri, currj, t = que.popleft()
+            time = max(time , t)
+            for r,c in dirc:
+                dr = r+curri
+                dc= c+currj
+                if 0<= dr < m and 0<= dc<n and grid[dr][dc]==1:
+                    grid[dr][dc]=2
+                    que.append((dr, dc, t+1))
+                    fresh_or-=1
+            
         return time if fresh_or==0 else -1
 
 
