c51d7ab6af8d627984958d962f4b54215cf10239
Shubhangi Pandey <35404670+shubhu15@users.noreply.github.com>
2026-01-04 16:15:55 -0800

    Time: 83 ms (48.45%) | Memory: 18.2 MB (57.74%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,33 +1,39 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
 
-        mapT = Counter(t)
-        want = len(mapT)
+        from collections import Counter
+
+        mapt = Counter(t)
+        need= len(mapt)
+
+        if len(t)> len(s):
+            return ""
         mapS={}
+        have=0
+        minl =float("inf")
+        res=[-1,-1]
 
-        have =0
-        minl = len(s)
-        mins =""
         i=0
-        j=0
-        while j<len(s):
-            mapS[s[j]] = 1+ mapS.get(s[j], 0) 
-            if s[j] in mapT and mapS[s[j]] == mapT[s[j]]:
-                have +=1
+        for j in range(len(s)):
+            mapS[s[j]] = mapS.get(s[j], 0) +1
 
-            while have == want:
-                if j-i+1 <= minl:
-                    minl = min(minl, j-i+1)
-                    mins= s[i:j+1]
+            if s[j] in mapt and mapS[s[j]] == mapt[s[j]] :
+                have+=1
 
+            while have == need:
+                if j-i+1 <minl:
+                    res= [i,j+1]
+                    minl= j-i+1
+                
+                if s[i] in mapt and mapS[s[i]] == mapt[s[i]]:
+                    have-=1
                 mapS[s[i]]-=1
-                if s[i] in mapT and mapS[s[i]]<mapT[s[i]]:
-                    have -=1
                 if mapS[s[i]]==0:
                     del mapS[s[i]]
                 i+=1
-            j+=1
-        return mins
+        return s[res[0]:res[1]] if minl!=float("inf") else ""
+
+
 
 
 
