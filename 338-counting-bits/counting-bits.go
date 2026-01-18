e87d3c911490f91c3ead6200faea4e2b95b4835a
Shubhangi Pandey <35404670+shubhu15@users.noreply.github.com>
2026-01-17 16:07:34 -0800

    Time: 0 ms (100.00%) | Memory: 6.4 MB (80.25%) - LeetSync

--- 338-counting-bits/counting-bits.go
+++ 338-counting-bits/counting-bits.go
@@ -1,22 +1,12 @@
 func countBits(n int) []int {
 
-    arr := make([]int,0, n+1)
-    k:=0
+    ans := make([]int, n+1)
 
+    for i:=1; i<=n; i++ {
+        ans[i] = ans[i>>1] + i&1
 
-    for k<n+1 {
-        cnt :=0
-        temp:=k
-        for temp!=0{
-            if (temp&1) == 1 {
-                cnt++
-
-            }
-            temp=temp>>1
-        }
-        arr = append(arr, cnt)
-        k++
     }
-    return arr
+
+    return ans
     
 }
